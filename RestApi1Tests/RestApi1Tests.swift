df745c076625966048de8c893388a599023e18f1
Vidya Ramamurthy <BridgeLabz@vidyas-mac-mini.local>
2016-01-13 12:15:13 +0530

    Initial Commit

---
+++ RestApi1Tests/RestApi1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  RestApi1Tests.swift
+//  RestApi1Tests
+//
+//  Created by Vidya Ramamurthy on 13/01/16.
+//  Copyright © 2016 BridgeLabz. All rights reserved.
+//
+
+import XCTest
+@testable import RestApi1
+
+class RestApi1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
