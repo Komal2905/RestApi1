8de8fb846b38adac80e22fc9ea84cdc6064212c4
Vidya Ramamurthy <BridgeLabz@Vidyas-Mac-mini.local>
2016-02-27 14:17:42 +0530

    A

--- RestApi1/ViewController.swift
+++ RestApi1/ViewController.swift
@@ -4,12 +4,10 @@
 //
 //  Created by Vidya Ramamurthy on 13/01/16.
 //  Copyright © 2016 BridgeLabz. All rights reserved.
-//
 // First Commit
-
-// This is Local changes which will be push on branch Master1
-// changed Local2
-// This line added on GitHub in branch Master1
+// This is Local changes which will be push on branch Master
+// changed Local
+// This line added on GitHub in branch Master
 //Ned
 
 
